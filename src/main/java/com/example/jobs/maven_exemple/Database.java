c7a1c52583aadf97286f120224263dc930c87d48
Khaliqi salma <khaliqisalma@gmail.com>
2026-09-18 23:15:48 +0100

    Update Database.java

--- src/main/java/com/example/jobs/maven_exemple/Database.java
+++ src/main/java/com/example/jobs/maven_exemple/Database.java
@@ -4,8 +4,8 @@ import java.sql.*;
 
 
 public class Database {
-	private static final String URL = "jdbc:mysql://localhost:3308/scrapingDB";
-    private static final String USER = "root";
+	private static final String URL = "jdbc:mysql://localhost:***/scrapingDB";
+    private static final String USER = "";
     private static final String PASSWORD = "";     
 
     public static Connection getConnection() throws SQLException {
