56775940dd1a4795789983946ed2609a6bb112e2
Khaliqi salma <khaliqisalma@gmail.com>
2026-09-18 23:29:53 +0100

    Update ChromeDriver path to relative reference

--- src/main/java/com/example/jobs/maven_exemple/JobzynScraper.java
+++ src/main/java/com/example/jobs/maven_exemple/JobzynScraper.java
@@ -13,7 +13,7 @@ public class JobzynScraper {
 
         // Path to ChromeDriver
         System.setProperty("webdriver.chrome.driver",
-                "C:\\Users\\SALMA.KH\\Downloads\\chromedriver.exe");
+                "chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
         WebDriverWait wait = new WebDriverWait(driver, 30);
